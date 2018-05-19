5e271c40b7619bc95e85b36169994ec35bf67b92
Sergiu Frunza <sergiufrunza@Sergius-MacBook-Pro.local>
2018-05-19 15:59:21 -0400

    some css and layout fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,19 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { host: 'https://localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['frunza.sergiu3@gmail.com'],
+  :password => ENV['sergiu6634'],
+  :domain => 'localhost:3000',
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
